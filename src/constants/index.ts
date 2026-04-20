a3ca2c1510966c71f1cc77a2c476c51149378ab9
Vladislav Galkin <lanqvist69@gmail.com>
2026-04-20 14:30:18 +0300

    Update from Vibe Coding Tool

--- src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export const LOCAL_STORAGE_NOTES_KEY = "notes";
-
-export const ACTIONS_TYPE = {
-  getNotes: "getNotes",
-  addNote: "addNote",
-  deleteNote: "deleteNote",
-  setPosition: "setPosition",
-  setText: "setText",
-};
